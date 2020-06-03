9ea4a77c50d2591ea80fa3c26dd2c031be4da178
Reinaldo Trindade <rewifetri@gmail.com>
2020-06-03 16:10:35 -0300

    FeedRoute reafctor

--- src/routes/FeedRoute/FeedRoute.jsx
+++ src/routes/FeedRoute/FeedRoute.jsx
@@ -1,53 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './FeedRoute.scss';
+import React, { useState, useEffect } from "react";
+import "./FeedRoute.scss";
 //
-import { fetchPosts } from "../../services/api"
-import { randomizePosts } from "../../services/utils"
-import Loading from '../../components/Loading';
-import Posts from '../../containers/Posts';
-import Stories from '../../containers/Stories';
-
+import { fetchPosts } from "../../services/api";
+import { randomizePosts } from "../../services/utils";
+import Loading from "../../components/Loading";
+import Posts from "../../containers/Posts";
+import Stories from "../../containers/Stories";
 
 const FeedRoute = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(null);
   const [posts, setPosts] = useState([]);
   const [stories, setStories] = useState([]);
-  const [page, setPage] = useState(1);
 
-  const getUserPostById = (postUserId) => users.find(user => postUserId === user.id);
+  const getUserById = (postUserId) =>
+    users.find((user) => postUserId === user.id);
 
   useEffect(() => {
     let stories;
-    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories')
-      .then(async (data) => {
-        stories = await data.json()
-      })    
-    fetchPosts()
-      .then(({ users, posts }) => {
-        posts =  randomizePosts(posts)
-        setStories(stories)
-        setUsers(users)
-        setPosts(posts)
-      })
-  }, [])
+    fetch("https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories").then(
+      async (data) => {
+        stories = await data.json();
+      }
+    );
+    fetchPosts().then(({ users, posts }) => {
+      // The fake api results are ordered by user, so i created this function
+      // to "randomize" them making it more realistic
+      posts = randomizePosts(posts);
+      setStories(stories);
+      setUsers(users);
+      setPosts(posts);
+    });
+  }, []);
 
   return (
     <div data-testid="feed-route">
-      {(stories.length && users.length) && (
-        <Stories
-          stories={stories}
-          getUserHandler={getUserPostById}
-        />
+      {users ? (
+        <>
+          <Stories stories={stories} getUserById={getUserById} />
+          <Posts posts={posts} getUserById={getUserById} />
+        </>
+      ) : (
+        <Loading />
       )}
-
-      {!users.length
-        ? (<Loading />)
-        : (
-          <Posts
-            posts={posts}
-            getUserHandler={getUserPostById}
-          />)
-      }
     </div>
   );
 };
