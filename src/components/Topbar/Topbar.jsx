e556b62ec95025e980d4dab0b5f4271e40a1e70e
Reinaldo Trindade <rewifetri@gmail.com>
2020-06-03 13:48:51 -0300

    TopBar.js Structure

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,8 +7,25 @@ import { ReactComponent as LogoSvg } from '../../assets/img/instagram-logo.svg';
 import './Topbar.scss';
 
 const Topbar = () => (
-  <header className="topbar">
+  <header className="topbar" data-testid="topbar">
     <div className="container">
+      <Link to="/" className="topbar__logo">
+        <LogoSvg alt="Logo Instagram" />
+      </Link>
+      <div className="topbar__group">
+        <button className="topbar__icon">
+          <Link to="/users">
+            <i className="fas fa-users" />
+            <span>Users</span>
+          </Link>
+        </button>
+        <button className="topbar__icon">
+          <Link to="/newuser">
+            <i className="fas fa-user-plus" />
+            <span>New Account</span>
+          </Link>
+        </button>
+      </div>
     </div>
   </header>
 );
