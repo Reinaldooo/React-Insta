8bb7c32a56bb223401a863a1c702a667e7941b71
Reinaldo Trindade <rewifetri@gmail.com>
2020-06-03 13:52:28 -0300

    Main route config

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import FeedRoute from './FeedRoute';
-import UsersRoute from './UsersRoute';
-import ProfileRoute from './ProfileRoute';
-import NewUserRoute from './NewUserRoute';
+import FeedRoute from "./FeedRoute";
+import NewUserRoute from "./NewUserRoute";
+import ProfileRoute from "./ProfileRoute";
+import UsersRoute from "./UsersRoute";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/">
-
+      <FeedRoute />
     </Route>
-
     <Route exact path="/users">
-
+      <UsersRoute />
     </Route>
-
     <Route path="/users/:username">
-
+      <ProfileRoute />
     </Route>
-
     <Route path="/newuser">
-
+      <NewUserRoute />
     </Route>
+    {/* 404 page */}
+    <Route
+      render={() => (
+        <div className="error">
+          Sorry, we could not find the page you are looking for!
+        </div>
+      )}
+    />
   </Switch>
 );
 
