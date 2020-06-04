b9ab8153d9ff76cb446dad7510fe9a28f965ea1d
Reinaldo Trindade <rewifetri@gmail.com>
2020-06-04 12:28:45 -0300

    Stories structure

--- src/containers/Stories/Stories.jsx
+++ src/containers/Stories/Stories.jsx
@@ -1,22 +1,56 @@
 import React, { useState } from "react";
-
+import './Stories.scss';
+//
 import Story from '../../components/Story';
 
-import './Stories.scss';
+const Stories = ({ stories, getUserById }) => {
+  const [showStory, setShowStory] = useState(false);
+  const [currentStory, setCurrentStory] = useState({});
+  const [selectedUser, setSelectedUser] = useState({});
 
-const Stories = ({ stories, getUserHandler }) => {
+  const findStoryById = (id) => stories.find(story => story.id === id);
+
+  const handleStory = (story, user) => {
+    story = findStoryById(story.id);
+    setSelectedUser(user);
+    setCurrentStory(story);
+    setShowStory(!showStory);
+  };
+
   return (
-    <React.Fragment>
-      <section className="stories">
+    <>
+      <section className="stories" data-testid="stories">
         <div className="container">
+          {
+            stories.map((story, index) => {
+              const user = getUserById(story.userId);
+              console.log(user)
+              return (
+                <div key={story.id} className="stories__story">
+                <button                  
+                  onClick={() => handleStory(story, user)}
+                  className={`user__thumb ${index === 0 && 'user__thumb--hasNew'}`}
+                >
+                  <div className="user__thumb__wrapper">
+                    <img src={user.avatar} alt={user.name} />
+                  </div>
+                </button>
+                <span>{user.username}</span>
+                </div>
+            )})
+          }
         </div>
       </section>
-
-      {showStory && (
-        <Story />
-        )}
-    </React.Fragment>
+      {
+      showStory && (
+        <Story
+          story={currentStory}
+          user={selectedUser}
+          handleClose={() => setShowStory(!showStory)}
+        />
+        )
+      }
+    </>
   );
 };
-
 export default Stories;
